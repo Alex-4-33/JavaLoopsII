d9fe84f69f9e159acd4a65f5c90c0ba30549ac9c
Alex Doe <cradle669@yandex.ru>
2021-01-03 20:32:34 +0400

    Initial commit

---
+++ src/main/java/JavaLoops.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class JavaLoops {
+
+    public static void printSeq(int a, int b, int n) {
+        int currentValue; //Текущий член последовательности для вывода
+        int currentPowerOf2; //Текущая степень двойки для вычисления след. члена
+
+        currentValue = a;
+        currentPowerOf2 = 1;
+        for (int i = 0; i < n; i++) {
+            //Вычисляем след. член
+            //currentValue = currentValue + currentPowerOf2 * b;
+            //currentPowerOf2 = currentPowerOf2 * 2;
+            currentValue += currentPowerOf2 * b;
+            currentPowerOf2 *= 2;
+
+            System.out.print(currentValue + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter q:");
+        int q = scan.nextInt();
+
+        for (int j = 0; j < q; j++) {
+            System.out.println("Enter a:");
+            int a = scan.nextInt();
+            System.out.println("Enter b:");
+            int b = scan.nextInt();
+            System.out.println("Enter n:");
+            int n = scan.nextInt();
+            System.out.println("Series for a = " + a + ", b = " + b + ", n = " + n + ":");
+            printSeq(a, b, n);
+        }
+
+    }
+}
